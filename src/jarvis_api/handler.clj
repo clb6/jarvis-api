c155d3c86b7caf15b1c61f2418b8ff5bc678ff82
Michael Hwang <hirehwang@gmail.com>
2016-02-02 00:08:41 -0500

    Stub the Jarvis endpoints on getting log entries

--- src/jarvis_api/handler.clj
+++ src/jarvis_api/handler.clj
@@ -3,18 +3,30 @@
             [ring.util.http-response :refer :all]
             [schema.core :as s]))
 
-(s/defschema Message {:message String})
+(s/defschema LogEntry {:body String})
+
+(defn query-log-entries
+  [tag search-term]
+  [{:body (str "query: " tag ", " search-term)}])
+
+(defn get-log-entry
+  [id]
+  {:body (str "get: " id)})
 
 (defapi app
   (swagger-ui)
   (swagger-docs
     {:info {:title "Jarvis-api"
-            :description "Compojure Api example"}
-     :tags [{:name "hello", :description "says hello in Finnish"}]})
-  (context* "/hello" []
-    :tags ["hello"]
+            :description "Jarvis data api"}
+     :tags [{:name "logentries", :description "handles Jarvis log entries"}]})
+  (context* "/logentries" []
+    :tags ["logentries"]
+    :summary "API to handle log entries"
     (GET* "/" []
-      :return Message
-      :query-params [name :- String]
-      :summary "say hello"
-      (ok {:message (str "Terve, " name)}))))
+      :return [LogEntry]
+      :query-params [{tag :- String ""}
+                     {searchterm :- String ""}]
+      (ok (query-log-entries tag searchterm)))
+    (GET* "/:id" [id]
+      :return LogEntry
+      (ok (get-log-entry id)))))
