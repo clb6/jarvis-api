ea8749b27e742836cad6a8ce2de424a62ca11279
Michael Hwang <hirehwang@gmail.com>
2016-02-02 22:59:05 -0500

    Hook in actually reading Jarvis log entries

--- src/jarvis_api/handler.clj
+++ src/jarvis_api/handler.clj
@@ -1,17 +1,28 @@
 (ns jarvis-api.handler
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [clojure.string :as cs]))
 
-(s/defschema LogEntry {:body String})
+(s/defschema LogEntry { :metadata String :body String })
+
+(def jarvis-root-directory (System/getenv "JARVIS_DIR_ROOT"))
+(def jarvis-log-directory (cs/join "/" [jarvis-root-directory "LogEntries"]))
 
 (defn query-log-entries
   [tag search-term]
   [{:body (str "query: " tag ", " search-term)}])
 
-(defn get-log-entry
+(defn parse-file
+  [text]
+  (let [[metadata body] (cs/split text #"\n\n")]
+    { :metadata metadata :body body }))
+
+(defn get-log-entry!
   [id]
-  {:body (str "get: " id)})
+  (let [log-entry-path (format "%s/%s.md" jarvis-log-directory id)
+        log-entry (slurp log-entry-path)]
+    (parse-file log-entry)))
 
 (defapi app
   (swagger-ui)
@@ -29,4 +40,4 @@
       (ok (query-log-entries tag searchterm)))
     (GET* "/:id" [id]
       :return LogEntry
-      (ok (get-log-entry id)))))
+      (ok (get-log-entry! id)))))
