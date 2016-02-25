b6d1a9f1d80f9cf46a83db4ec142fa87b59443d5
Michael Hwang <hirehwang@gmail.com>
2016-02-25 12:10:27 -0500

    Fix: put back `optional-key` in LogEntryRequest
    This got lost when moved over to the dedicated namespace.

--- src/jarvis_api/schemas.clj
+++ src/jarvis_api/schemas.clj
@@ -16,8 +16,8 @@
 (s/defschema LogEntryRequest  { :author String
                                :occurred String
                                :tags [s/Str]
-                               :parent (s/maybe s/Str)
-                               :todo (s/maybe s/Str)
+                               (s/optional-key :parent) (s/maybe s/Str)
+                               (s/optional-key :todo) (s/maybe s/Str)
                                :setting String
                                :body String })
 
