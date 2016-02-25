8ea8f94833520e8065eb5874f17ee23f25d36727
Michael Hwang <hirehwang@gmail.com>
2016-02-25 11:31:24 -0500

    Move schemas to their own namespace

--- src/jarvis_api/handler.clj
+++ src/jarvis_api/handler.clj
@@ -2,40 +2,9 @@
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
             [schema.core :as s]
+            [jarvis-api.schemas :refer [LogEntry LogEntryRequest Tag TagRequest]]
             [jarvis-api.resources.tags :as tags]
             [jarvis-api.resources.logentries :as logs]))
-
-
-(s/defschema LogEntry { :author String
-                       :created String
-                       :occurred String
-                       :version String
-                       :tags [s/Str]
-                       :parent (s/maybe s/Str)
-                       :todo (s/maybe s/Str)
-                       :setting String
-                       :body String })
-
-(s/defschema LogEntryRequest  { :author String
-                               :occurred String
-                               :tags [s/Str]
-                               (s/optional-key :parent) (s/maybe s/Str)
-                               (s/optional-key :todo) (s/maybe s/Str)
-                               :setting String
-                               :body String })
-
-(s/defschema Tag { :author s/Str
-                  :created s/Str
-                  :version s/Str
-                  :tags [s/Str]
-                  :body s/Str })
-
-(s/defschema TagRequest { :name s/Str
-                         :author s/Str
-                         :tags [s/Str]
-                         :body s/Str })
-
-(s/defschema WebError { :message s/Str })
 
 
 (defn query-log-entries
---
+++ src/jarvis_api/schemas.clj
@@ -0,0 +1,34 @@
+(ns jarvis-api.schemas
+  (:require [schema.core :as s]))
+
+
+(s/defschema LogEntry { :author String
+                       :created String
+                       :occurred String
+                       :version String
+                       :tags [s/Str]
+                       :parent (s/maybe s/Str)
+                       :todo (s/maybe s/Str)
+                       :setting String
+                       :body String })
+
+
+(s/defschema LogEntryRequest  { :author String
+                               :occurred String
+                               :tags [s/Str]
+                               :parent (s/maybe s/Str)
+                               :todo (s/maybe s/Str)
+                               :setting String
+                               :body String })
+
+
+(s/defschema Tag { :author s/Str
+                  :created s/Str
+                  :version s/Str
+                  :tags [s/Str]
+                  :body s/Str })
+
+(s/defschema TagRequest { :name s/Str
+                         :author s/Str
+                         :tags [s/Str]
+                         :body s/Str })
