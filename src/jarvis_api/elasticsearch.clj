a23154ce830f7cf4112a748562ee7d6f3956f044
Michael Hwang <hirehwang@gmail.com>
2016-03-16 23:16:05 -0400

    Add get and delete calls

--- src/jarvis_api/elasticsearch.clj
+++ src/jarvis_api/elasticsearch.clj
@@ -8,3 +8,15 @@
   [document-type document-id document]
   (let [conn (esr/connect config/jarvis-elasticsearch-uri)]
     (esd/put conn "jarvis" document-type document-id document)))
+
+(defn get-jarvis-document
+  [document-type document-id]
+  (let [conn (esr/connect config/jarvis-elasticsearch-uri)
+        response (esd/get conn "jarvis" document-type document-id)]
+    (if (:found response)
+      (:_source response))))
+
+(defn delete-jarvis-document
+  [document-type document-id]
+  (let [conn (esr/connect config/jarvis-elasticsearch-uri)]
+    (esd/delete conn "jarvis" document-type document-id)))
