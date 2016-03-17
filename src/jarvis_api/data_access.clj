28259d710d06a0ebab7354e7f797509450d2b094
Michael Hwang <hirehwang@gmail.com>
2016-03-16 23:18:10 -0400

    Add rollback ability in write-jarvis-document

--- src/jarvis_api/data_access.clj
+++ src/jarvis_api/data_access.clj
@@ -1,9 +1,39 @@
 (ns jarvis-api.data_access
-  (:require [jarvis-api.elasticsearch :as jes]))
+  (:require [jarvis-api.elasticsearch :as jes]
+            [clojure.java.io :as io]))
 
-(defn write-jarvis-document!
+
+(defn- delete-jarvis-document!
+  ; TODO: Better error checking and returning. The delete-file throws exceptions.
+  [document-type file-path document-id]
+  (jes/delete-jarvis-document document-type document-id)
+  (io/delete-file file-path))
+
+(defn- write-jarvis-document-unsafe!
   [document-type file-path create-file-representation document-id document]
-  ; TODO: Handle unexpected errors and parseable errors
   (jes/put-jarvis-document document-type document-id document)
   (spit file-path (create-file-representation document))
   document)
+
+(defn write-jarvis-document!
+  [document-type file-path create-file-representation document-id document]
+  (let [document-prev (jes/get-jarvis-document document-type document-id)]
+    (try
+      (write-jarvis-document-unsafe! document-type file-path
+                                          create-file-representation
+                                          document-id
+                                          document)
+      (catch Exception e
+        (println (str "Error writing document: " (.getMessage e)))
+        ; Try to rollback changes
+        (try
+          (if document-prev
+            (write-jarvis-document-unsafe! document-type file-path
+                                                create-file-representation
+                                                document-id
+                                                document-prev)
+            (delete-jarvis-document! document-type file-path document-id))
+          (catch Exception e
+            (println (str "Error rolling back: " (.getMessage e)))))
+        ; TODO: Remove this throw and handle better at higher level
+        (throw e)))))
