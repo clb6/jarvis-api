c93dfc592544db58acd257e5ab41cc3f3b21033b
Michael Hwang <hirehwang@gmail.com>
2016-06-01 07:12:59 -0400

    Implement JARVIS_DATA_VERSION

--- bin/setup_elasticsearch.sh
+++ bin/setup_elasticsearch.sh
@@ -8,10 +8,10 @@ echo "Elasticsearch URL: $URL_ELASTICSEARCH"
 
 DIR_MAPPINGS="setup/mappings"
 
-curl -X PUT $URL_ELASTICSEARCH/jarvis-danish
-curl -X PUT $URL_ELASTICSEARCH/jarvis-danish/_mapping/tags -d \
+curl -X PUT $URL_ELASTICSEARCH/jarvis-$JARVIS_DATA_VERSION
+curl -X PUT $URL_ELASTICSEARCH/jarvis-$JARVIS_DATA_VERSION/_mapping/tags -d \
     @$DIR_MAPPINGS/mapping_tags.json
-curl -X PUT $URL_ELASTICSEARCH/jarvis-danish/_mapping/logentries -d \
+curl -X PUT $URL_ELASTICSEARCH/jarvis-$JARVIS_DATA_VERSION/_mapping/logentries -d \
     @$DIR_MAPPINGS/mapping_log_entries.json
 # Moving away from alias but keep here for now
-#curl -X PUT $URL_ELASTICSEARCH/jarvis-danish/_alias/jarvis
+#curl -X PUT $URL_ELASTICSEARCH/jarvis-$JARVIS_DATA_VERSION/_alias/jarvis
--- src/jarvis_api/config.clj
+++ src/jarvis_api/config.clj
@@ -2,15 +2,23 @@
   (:require [clojure.string :as cs]))
 
 
+(def jarvis-data-version (System/getenv "JARVIS_DATA_VERSION"))
+
+(defn- versionize-name
+  [some-name version]
+  (cs/join "-" [some-name version]))
+
 (def jarvis-root-directory (System/getenv "JARVIS_DIR_ROOT"))
-(def jarvis-log-directory (cs/join "/" [jarvis-root-directory "LogEntries"]))
+(def jarvis-log-directory (cs/join "/" [jarvis-root-directory
+                                        (versionize-name "LogEntries"
+                                                         jarvis-data-version)]))
 (def jarvis-images-directory (cs/join "/" [jarvis-root-directory "Images"]))
-(def jarvis-tag-directory (cs/join "/" [jarvis-root-directory "Tags"]))
+(def jarvis-tag-directory (cs/join "/" [jarvis-root-directory
+                                        (versionize-name "Tags" jarvis-data-version)]))
 
 (def jarvis-tag-version "0.3.0")
 (def jarvis-log-entry-version "0.7.0")
 
 (def jarvis-elasticsearch-uri (or (System/getenv "JARVIS_ELASTICSEARCH")
                                   "http://elasticsearch.jarvis.home:9200"))
-; Versioned by breakfast food
-(def jarvis-elasticsearch-index "jarvis-danish")
+(def jarvis-elasticsearch-index (versionize-name "jarvis" jarvis-data-version))
