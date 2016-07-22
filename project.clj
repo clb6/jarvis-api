3849a7423ae95e39ae4216bc04bb449d10cc2013
Michael Hwang <hirehwang@gmail.com>
2016-07-22 01:21:23 -0400

    Bump version to 0.13.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarvis-api "0.12.0"
+(defproject jarvis-api "0.13.0"
   :description "Data API for Jarvis"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.11.0"] ; required due to bug in lein-ring
