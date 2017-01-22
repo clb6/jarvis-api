3b8f6cb2a1fa4f72ab7830fa05169c3fc6f57d6b
Michael Hwang <hirehwang@gmail.com>
2017-01-22 02:48:44 -0500

    WIP: Start removing old stuff

--- src/jarvis_api/data_access/common.clj
+++ src/jarvis_api/data_access/common.clj
@@ -16,38 +16,6 @@
     (do (spit file-path (create-file-representation document))
         document)))
 
-
-(defn write-jarvis-document!
-  "First impl just simply calls PUT on Elasticsearch. Second impl is a compound
-  transaction of trying to write to file and to Elasticsearch or not."
-  ([document-type document-id document]
-   (jes/put-jarvis-document document-type document-id document))
-  ([document-type file-path create-file-representation document-id document]
-  (let [document-prev (jes/get-jarvis-document document-type document-id)]
-    (letfn [(rollback []
-              (try
-                (if document-prev
-                  (write-jarvis-document-unsafe! document-type file-path
-                                                 create-file-representation
-                                                 document-id
-                                                 document-prev)
-                  (delete-jarvis-document! document-type file-path document-id))
-                (catch Exception e
-                  (log/error (str "Error rolling back: " (.getMessage e))))))]
-      (try
-        (if-let [document-written (write-jarvis-document-unsafe! document-type file-path
-                                                                 create-file-representation
-                                                                 document-id
-                                                                 document)]
-          document-written
-          (do (log/error (str "Document failed to write: " document-id))
-              (rollback)))
-      (catch Exception e
-        (log/error (str "Error writing document: " (.getMessage e)))
-        ; Try to rollback changes
-        (rollback)
-        ))))))
-
 (defn get-jarvis-document!
   [document-type document-id]
   (jes/get-jarvis-document document-type document-id))
